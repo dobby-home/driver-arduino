edfb1d3e48db7fb7858af5cec0df27951a137a2a
Oleg Mikhaylenko <blaremc@gmail.com>
2014-02-25 01:51:34 +0400

    Set value and get value with help HTTP for driver Arduino

--- classes/Driver/Arduino.php
+++ classes/Driver/Arduino.php
@@ -8,8 +8,46 @@ class Driver_Arduino extends Dobby_Driver {
         $address = $this->parseAddress($address);
         $value = null;
         if ($address[0] == 'http') {
-            $value = file_get_contents('http://' . $address[1] . '/get' . ucfirst($address[2]));
+
+            $url = 'http://' . $address[1] . '/get' . ucfirst($address[2]);
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->_timeout);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+
+            $value = curl_exec($ch);
+
+            $error = curl_error($ch);
+            if (!empty($error)) {
+                throw new Exception($error);
+            }
+            curl_close($ch);
         }
         return $value;
+    }
+
+
+    public function setValue($address, $value) {
+
+        $address = $this->parseAddress($address);
+        if ($address[0] == 'http') {
+
+            $url = 'http://' . $address[1] . '/set' . ucfirst($address[2]) . ':' . $value;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_TIMEOUT, $this->_timeout);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+
+            $res = curl_exec($ch);
+
+            $error = curl_error($ch);
+            if (!empty($error)) {
+                throw new Exception($error);
+            }
+            curl_close($ch);
+        }
     }
 }
